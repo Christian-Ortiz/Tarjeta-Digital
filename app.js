6f632376fef0e77f8406109e27f00b6c3d3a4d40
Christian-Ortiz <70713753+Christian-Ortiz@users.noreply.github.com>
2023-08-21 22:43:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ dots.forEach((li, key) => {
 
 //boton compartir
 const boton = document.querySelector('#compartir');
-const file = new File([], "./imagenes/qr.svg", { type: "image/svg" });
+const file = new File([], "imagenes/qr.png", { type: "image/png" });
 if ('share' in navigator) {
 	boton.addEventListener('click', share);
 	function share() {
@@ -196,7 +196,7 @@ if ('share' in navigator) {
 			title: 'Christian Jhulino Ortiz Cholán',
 			text: 'Técnico en Computación e Informática, Diseñador Gráfico y Desarrollador de Software...',
 			url: 'https://christian-ortiz.github.io/Tarjeta-Digital/',
-			files: [file]
+			files: [file],
 		})
 		.then(()=>{
 
